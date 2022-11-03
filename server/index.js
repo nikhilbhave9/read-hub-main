81eb87d563defde74d955b20a4c009bd0e114c60
Nikhil Bhave <nik.3731@gmail.com>
2022-11-03 11:24:53 +0530

    Add boilerplate Express code

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
-// Import: express, mongoose, 
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.get("/", function(req, res) {
+    res.send("Hello, world!");
+});
+
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log("Server is running at port " + port);
+});
