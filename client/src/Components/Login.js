9a6a8378c2d2f36d7f1bc98ab4428cbcc8f8f0e9
Nikhil Bhave <nik.3731@gmail.com>
2022-11-06 10:08:03 +0530

    Add Docker Compose

---
+++ client/src/Components/Login.js
@@ -0,0 +1,11 @@
+import react from 'react'
+
+function Login () {
+    return (
+        <div>
+            <input type="text" onChange={getInputValue}/>
+        </div>
+    );
+}
+
+export default Login;
