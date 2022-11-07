c7f5e97db7ad5c4dbc6421213bd8dfa4cc1f2fae
Soham Bagchi <sohambagchi@outlook.com>
2022-11-07 14:49:58 +0530

    scraper mongodb connection

--- scraper/network_utils.py
+++ scraper/network_utils.py
@@ -1,2 +1,17 @@
-def initialize_mongo_connection():
-    
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+def load_environment_and_connect_client():
+    global client
+    load_dotenv()
+    username = os.getenv("MONGO_USERNAME")
+    password = os.getenv("MONGO_PASSWORD")
+    cluster_address = os.getenv("MONGO_CLUSTER_ADDRESS")
+    client = MongoClient(f"mongodb+srv://{username}:{password}@{cluster_address}/test?retryWrites=true&w=majority")
+
+# initialize a mongodb atlas connection
+def connect_mongo_db_atlas(db_name, collection_name):
+    global client
+    db = client[db_name]
+    collection = db[collection_name]
+    return collection
