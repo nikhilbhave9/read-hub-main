2f17506d5f9bf3a2d50a6b6f6ec73d8534a680ad
Nikhil Bhave <nik.3731@gmail.com>
2022-11-08 13:12:18 +0530

    Set up MongoDB Atlas GET

--- server/index.js
+++ server/index.js
@@ -1,35 +1,29 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import cors from 'cors';
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const Article = require('./models/articles');
 
-const dbo = require('./db/conn');
+const url = `mongodb+srv://admin:root@cluster0.s8asufw.mongodb.net/?retryWrites=true&w=majority`;
+
+// const connectionParams={
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true 
+// }
+mongoose.connect(url)
+    .then(() => {
+        console.log('Connected to database ')
+    })
+    .catch((err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    })
 
 const app = express();
-// const router = express.Router();
 app.use(cors({ origin: true, credentials: true }));
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 8000;
-
-// const rssUrlSchema = new mongoose.Schema({
-//     name: String,
-//     url: String,
-//     description: String,
-// })
-
-// const rssUrl = mongoose.model('rssUrl', rssUrlSchema);
-
-dbo.connectToServer(function(err) {
-    if (err) {
-        console.error(err);
-        process.exit()
-    }
-
-    app.listen(port, () => {
-        console.log("Server is running at port " + port);
-    });
-})
 
 app.get("/", function(req, res) {
     res.send("Hello, world!");
@@ -45,3 +39,39 @@ app.post("/api2", function(req, res) {
     console.log(rss_url.name)
 
 })
+
+app.get("/api3", function(req, res) {
+    Article.find()
+        .then((result) => {
+            res.json(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+
+});
+
+
+
+// const dbo = require('./db/conn');
+
+// const rssUrlSchema = new mongoose.Schema({
+//     name: String,
+//     url: String,
+//     description: String,
+// })
+
+// const rssUrl = mongoose.model('rssUrl', rssUrlSchema);
+
+// dbo.connectToServer(function(err) {
+//     if (err) {
+//         console.error(err);
+//         process.exit()
+//     }
+
+//     
+// })
+
+app.listen(port, () => {
+    console.log("Server is running at port " + port);
+});
---
+++ server/models/articles.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const articleSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    }, 
+    body: {
+        type:  String,
+        required: true
+    }
+});
+
+const Article = mongoose.model('Article', articleSchema);
+module.exports = Article;
