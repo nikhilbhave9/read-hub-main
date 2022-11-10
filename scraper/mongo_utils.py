61bf42e56df9ffc02021a1a511c863d53dc518b1
Soham Bagchi <sohambagchi@outlook.com>
2022-11-10 13:39:00 +0530

    mongo utils is now a class

--- scraper/mongo_utils.py
+++ scraper/mongo_utils.py
@@ -5,50 +5,55 @@ from pprint import pprint
 
 import os
 
+class MongoUtils:
+    def __init__(self, location="remote"):
+        self.client = self.load_environment_and_connect_client(location)
 
-def load_environment_and_connect_client():
-    load_dotenv()
-    username = os.getenv("MONGO_USERNAME")
-    password = os.getenv("MONGO_PASSWORD")
-    # this is the only way to make inter-container mongo communication work apparently
-    # instead of '@localhost:27017' use the name of the container instance, which should be this only
-    client = MongoClient(f"mongodb://{username}:{password}@read-hub-mongo-1/") 
-    # client = MongoClient('mongodb+srv://admin:root@cluster0.s8asufw.mongodb.net/?retryWrites=true&w=majority')
-    return client
+    def load_environment_and_connect_client(self, location):
+        load_dotenv()
+        username = os.getenv("MONGO_USERNAME")
+        password = os.getenv("MONGO_PASSWORD")
+        # this is the only way to make inter-container mongo communication work apparently
+        # instead of '@localhost:27017' use the name of the container instance, which should be this only
+        if location=="local":
+            client = MongoClient(f"mongodb://{username}:{password}@read-hub-mongo-1/") 
+        elif location=="remote":
+            client = MongoClient('mongodb+srv://admin:root@cluster0.s8asufw.mongodb.net/?retryWrites=true&w=majority')
+        return client
 
-# This function does two levels of sanitization to
-# handle FastAPI's incompetency. First it removes the 
-# key '_id' from the object, because FastAPI doesn't
-# know how parse a key with underscore, then it 
-# stringify's the value of that field because the 
-# ObjectId() type is not JSON serializable
-def sanitize_mongo_object(obj):
-    obj['id'] = str(obj.pop("_id"))
-    return obj
+    # This function does two levels of sanitization to
+    # handle FastAPI's incompetency. First it removes the 
+    # key '_id' from the object, because FastAPI doesn't
+    # know how parse a key with underscore, then it 
+    # stringify's the value of that field because the 
+    # ObjectId() type is not JSON serializable
+    def sanitize_mongo_object(self, obj):
+        obj['id'] = str(obj.pop("_id"))
+        return obj
 
-def sanitize_mongo_objects(objs):
-    return [sanitize_mongo_object(obj) for obj in objs]
+    def sanitize_mongo_objects(self, objs):
+        return [self.sanitize_mongo_object(obj) for obj in objs]
 
-# initialize a mongodb atlas connection
-def fetch_collection(client, db_name, collection_name):
-    db = client[db_name]
-    collection = db[collection_name]
-    return collection
+    # initialize a mongodb atlas connection
+    def fetch_collection(self, db_name, collection_name):
+        db = self.client[db_name]
+        collection = db[collection_name]
+        return collection
 
-def find_item_in_collection(collection, query):
-    return sanitize_mongo_object(collection.find_one(query)) 
+    def find_item_in_collection(self, collection, query):
+        return self.sanitize_mongo_object(collection.find_one(query)) 
 
-# insert item to mongodb collection
-def insert_item_to_collection(collection, item):
-    collection.insert_one(item)
-    return sanitize_mongo_object(collection.find_one(item)) 
+    # insert item to mongodb collection
+    def insert_item_to_collection(self, collection, item):
+        collection.insert_one(item)
+        return self.sanitize_mongo_object(collection.find_one(item)) 
 
-# insert list of items to mongodb collection
-def insert_items_to_collection(collection, items):
-    collection.insert_many(items)
-    return sanitize_mongo_objects(collection.find({}))
+    # insert list of items to mongodb collection
+    def insert_items_to_collection(self, collection, items):
+        collection.insert_many(items)
+        return self.sanitize_mongo_objects(collection.find({}))
 
-# update item in mongodb collection
-def update_item_in_collection(collection, query, item):
-    collection.update_one(query, {"$set": item}, upsert=True)
-    return sanitize_mongo_object(collection.find_one(query)) 
+    # update item in mongodb collection
+    def update_item_in_collection(self, collection, query, item):
+        collection.update_one(query, {"$set": item}, upsert=True)
+        return self.sanitize_mongo_object(collection.find_one(query)) 
