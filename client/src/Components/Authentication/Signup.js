1ff221a8b2743da6f57f2ec7adfadd9be18fe75b
Nikhil Bhave <nik.3731@gmail.com>
2022-11-24 18:03:05 +0530

    Add profile create section

--- client/src/Components/Authentication/Signup.js
+++ client/src/Components/Authentication/Signup.js
@@ -1,6 +1,7 @@
 // ============= WILL BE USED FOR FIRST-TIME SIGNUP =============
 
 // Imports
+import * as React from 'react';
 
 // Components
 
@@ -8,8 +9,15 @@
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { sizing } from '@mui/system';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import MenuItem from '@mui/material/MenuItem';
 
+// import dayjs from 'dayjs';
+// import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
+// import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+// import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 // Google OAuth sign up (if needed)
 
 
@@ -22,65 +30,141 @@ import { sizing } from '@mui/system';
 // Step 5: Add useNavigate hook from React Router to redirect to user-specific Dashboard 
 
 
+// List of Subscription tiers
+const subscriptions = [
+    {
+        value: '1',
+        label: 'Free',
+    },
+    {
+        value: '2',
+        label: 'Premium',
+    },
+    {
+        value: '3',
+        label: 'Premium Plus',
+    },
+];
+
+
 function Signup() {
+
+    // Configuration for Date Picker
+    // const [value, setValue] = React.useState(dayjs('2014-08-18T21:11:54'));
+
+    // const handleChange = (newValue) => {
+    //     setValue(newValue);
+    // };
+
+    // Handle Subscription tiers
+    const [subscription, setSubscription] = React.useState('1');
+
+    const handleChange = (event) => {
+        setSubscription(event.target.value);
+    };
+
+
     return (
         <Container align="center">
-            <Box
+            <Typography variant='h3'>Set up your profile</Typography>
+            <Stack
                 component="form"
-                sx={{
-                    '& .MuiTextField-root': { m: 1, width: '25ch' },
-                }}
+                // sx={{
+                //     '& .MuiTextField-root': { m: 1, width: '25ch' },
+                // }}
                 noValidate
                 autoComplete="off"
             >
-                <div>
-                    <TextField
-                        required
-                        id="firstname-required"
-                        label="First Name"
-                        fullWidth
+
+                <TextField
+                    fullWidth
+                    required
+                    id="firstname-required"
+                    label="First Name"
+                    margin="normal"
+                />
+                <TextField
+                    fullWidth
+                    id="lastname"
+                    label="Last Name"
+                    margin="normal"
+                />
+
+                <Button
+                    variant="contained"
+                    component="label"
+                >
+                    Upload Profile Photo
+                    <input
+                        type="file"
+                        hidden
                     />
-                    <TextField
-                        id="lastname"
-                        label="Last Name"
-                    />
-                    <TextField
-                        disabled
-                        id="outlined-disabled"
-                        label="Disabled"
-                        defaultValue="Hello World"
-                    />
-                    <TextField
-                        id="outlined-password-input"
-                        label="Password"
-                        type="password"
-                        autoComplete="current-password"
-                    />
-                    <TextField
-                        id="outlined-read-only-input"
-                        label="Read Only"
-                        defaultValue="Hello World"
-                        InputProps={{
-                            readOnly: true,
-                        }}
-                    />
-                    <TextField
-                        id="outlined-number"
-                        label="Number"
-                        type="number"
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
-                    <TextField id="outlined-search" label="Search field" type="search" />
-                    <TextField
-                        id="outlined-helperText"
-                        label="Helper text"
-                        defaultValue="Default Value"
-                        helperText="Some important text"
-                    />
-                </div>
-            </Box>
+                </Button>
+
+                <TextField
+                    id="dob"
+                    label="Date of Birth"
+                    type="date"
+                    defaultValue="2000-11-01"
+                    margin="normal"
+                    sx={{ width: 220 }}
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                />
+
+                {/* <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <MobileDatePicker
+                            label="Date of Birth"
+                            inputFormat="DD/MM/YYYY"
+                            value={value}
+                            onChange={handleChange}
+                            renderInput={(params) => <TextField {...params} />}
+
+                        />
+                    </LocalizationProvider> */}
+
+                <TextField
+                    id="outlined-subscription-select"
+                    select
+                    label="Tier"
+                    margin="normal"
+                    value={subscription}
+                    onChange={handleChange}
+                    helperText="Please select your preferred tier"
+                >
+                    {subscriptions.map((option) => (
+                        <MenuItem key={option.value} value={option.value}>
+                            {option.label}
+                        </MenuItem>
+                    ))}
+                </TextField>
+
+                <Typography variant="body1">Start off with up to 3 newsletter URLs</Typography>
+
+                <TextField
+                    fullWidth
+                    id="sub1"
+                    label="Subscription #1"
+                    margin="normal"
+                />
+                <TextField
+                    fullWidth
+                    id="sub2"
+                    label="Subscription #2"
+                    margin="normal"
+                />
+                <TextField
+                    fullWidth
+                    id="sub3"
+                    label="Subscription #3"
+                    margin="normal"
+                />
+                
+                <Button>
+                    Submit
+                </Button>
+            </Stack>
         </Container>
     )
 }
